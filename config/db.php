65cb76797ebad351ea8684385f0a5d81c1ae016c
Aleksandr Yasenkov <yasenkov.sasha@gmail.com>
2022-08-22 00:20:35 +0300

    first commit

--- config/db.php
+++ config/db.php
@@ -2,9 +2,9 @@
 
 /* Подключение к БД */
 define('DB_HOST', '127.0.0.1');
-define('DB_NAME', 'shop');
-define('DB_USER', 'root');
-define('DB_PASSWORD', 'sanek650');
+define('DB_NAME', '');
+define('DB_USER', '');
+define('DB_PASSWORD', '');
 
 $dbConnect = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
 
